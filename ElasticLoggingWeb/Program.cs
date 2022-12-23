253827c5d37e45e90ad7ebf44f280860473dc6bf
Ladislav Filip <ladislav.filip@gmail.com>
2022-12-23 08:02:00 +0100

    Konfigurace serilogu v settingu

--- ElasticLoggingWeb/Program.cs
+++ ElasticLoggingWeb/Program.cs
@@ -1,20 +1,8 @@
-using System.Reflection;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .Enrich.WithEnvironmentName()
-    .Enrich.WithMachineName()
-    .WriteTo.Console()
-    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]!))
-    {
-        AutoRegisterTemplate = true,
-        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
-    })
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
