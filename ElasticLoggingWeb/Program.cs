f7b7a2b1c825d0d37d9930a71c8b1414994e8b18
Ladislav Filip <ladislav.filip@gmail.com>
2022-12-28 12:35:24 +0100

    UI healthcheck

--- ElasticLoggingWeb/Program.cs
+++ ElasticLoggingWeb/Program.cs
@@ -1,4 +1,6 @@
 using ElasticLoggingWeb.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,7 @@ Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
-Log.Information("Start application v6...");
+Log.Information("Start application v10...");
 
 builder.Host
     .UseSerilog();
@@ -19,6 +21,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCustomSwaggerGen();
 builder.Services.AddCustomAuthentication(builder.Configuration);
+builder.Services.AddHealthChecks()
+    .AddProcessAllocatedMemoryHealthCheck(maximumMegabytesAllocated: 50, tags: new[] { "memory" }, name: "Process alocated memory")
+    .AddElasticsearch(cfg =>
+    {
+        var url = builder.Configuration["Serilog:WriteTo:0:Args:nodeUris"];
+        Log.Information("Elasticsearch URL address is '{Url}'", url);
+        cfg.UseServer(url);
+    }, name: "Elasticsearch")
+    ;
+
+// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+builder.Services
+    .AddHealthChecksUI(cfg =>
+    {
+        var healthPath = builder.Configuration.GetValue<string>("HEALTH_URL")!;
+        cfg.AddHealthCheckEndpoint("main", healthPath);
+        cfg.MaximumHistoryEntriesPerEndpoint(50);
+    })
+    .AddInMemoryStorage();
 
 var app = builder
     .Build();
@@ -27,12 +48,31 @@ app.UseCustomSwagger(builder.Configuration);
 
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.MapControllers();
+// app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app
+    .UseRouting()
+    .UseEndpoints(cfg =>
+    {
+        cfg.MapControllers();
+        cfg.MapHealthChecksUI(setup =>
+        {
+            setup.AddCustomStylesheet("wwwroot/health.css");
+        });
+    });
 
 app.Run();
